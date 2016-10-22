2878e5cc849fe9e66583504e566df9a0da147c5c
Catofes <i@catofes.com>
2016-10-22 10:29:56 +0800

    update

--- Route/file.py
+++ Route/file.py
@@ -18,6 +18,7 @@ class RFile:
                         "attachment; filename=\"%s\"" % quote(result['name'], encoding='utf-8'))
         resp.set_header("content-type", "application/octet-stream")
         resp.set_header("content-length", "length")
+        resp.set_header("cache-control","max-age=864000")
         if req.range:
             start = req.range(0)
             if req.range(1) > start:
